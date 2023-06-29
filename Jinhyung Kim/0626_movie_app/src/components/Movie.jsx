4f938f4c5244ad4ca0b3530554b4d97d5ce19fab
Jin <jinnkimm7@gmail.com>
2023-06-29 23:58:14 +0900

    Style : Movie Component

--- Jinhyung Kim/0626_movie_app/src/components/Movie.jsx
+++ Jinhyung Kim/0626_movie_app/src/components/Movie.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const MovieContainer = styled.div`
+  background-color: #f2f2f2;
+  padding: 20px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+`;
+
+const MovieTitle = styled.h4`
+  color: #333;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+const MovieYear = styled.h4`
+  color: #666;
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
+const MovieSummary = styled.p`
+  color: #888;
+  font-size: 14px;
+  margin-bottom: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+const MoviePoster = styled.img`
+  width: 100px;
+  height: auto;
+  margin-bottom: 10px;
+`;
 
 export default function Movie({ id, year, title, summary, poster }) {
   return (
-    <div id={id}>
-      <h4>{title}</h4>
-      <h4>{year}</h4>
-      <h4>{summary}</h4>
-      <h4>{poster}</h4>
-    </div>
+    <MovieContainer id={id}>
+      <MovieTitle>{title}</MovieTitle>
+      <MovieYear>{year}</MovieYear>
+      <MovieSummary>{summary}</MovieSummary>
+      <MoviePoster src={poster} alt={title} />
+    </MovieContainer>
   );
 }
 
